16c90ba242d9ac788ee676bc40c8dd62e81cd6d8
Laion Souza <laion.souza0507@gmail.com>
2024-11-02 13:21:14 -0300

    HTML

--- models/cliente.js
+++ models/cliente.js
@@ -2,6 +2,6 @@ import Conta from "./conta.js";
 
 export default class Cliente extends Conta {
     constructor(nome, numAgencia, saldo = 0) {
-        super(nome, numAgencia, saldo)
-    }
+        super(nome, numAgencia, saldo);
+    };
 }
